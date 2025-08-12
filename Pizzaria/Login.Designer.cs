c04be7b85b01ab8c2446068fc485966afb98b284
jVitor90 <joaooviitor2007>
2025-08-12 13:50:39 -0300

    Pizzaria

--- Pizzaria/Login.Designer.cs
+++ Pizzaria/Login.Designer.cs
@@ -1,16 +1,16 @@
 ﻿namespace Pizzaria
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
-        /// Variável de designer necessária.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Limpar os recursos que estão sendo usados.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,157 +20,20 @@
             base.Dispose(disposing);
         }
 
-        #region Código gerado pelo Windows Form Designer
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Método necessário para suporte ao Designer - não modifique 
-        /// o conteúdo deste método com o editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.linkblcadastrar = new System.Windows.Forms.LinkLabel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lblcadastrar = new System.Windows.Forms.Label();
-            this.lblsenha = new System.Windows.Forms.Label();
-            this.lblcpf = new System.Windows.Forms.Label();
-            this.txbcpf = new System.Windows.Forms.TextBox();
-            this.txbsenha = new System.Windows.Forms.TextBox();
-            this.btnentrar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(378, 35);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(159, 152);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 18;
-            this.pictureBox1.TabStop = false;
-            // 
-            // linkblcadastrar
-            // 
-            this.linkblcadastrar.AutoSize = true;
-            this.linkblcadastrar.BackColor = System.Drawing.Color.Transparent;
-            this.linkblcadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkblcadastrar.LinkColor = System.Drawing.Color.Lime;
-            this.linkblcadastrar.Location = new System.Drawing.Point(509, 375);
-            this.linkblcadastrar.Name = "linkblcadastrar";
-            this.linkblcadastrar.Size = new System.Drawing.Size(87, 20);
-            this.linkblcadastrar.TabIndex = 17;
-            this.linkblcadastrar.TabStop = true;
-            this.linkblcadastrar.Text = "Clique aqui";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(345, 375);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(160, 20);
-            this.label2.TabIndex = 16;
-            this.label2.Text = "Deseja se cadastrar?";
-            // 
-            // lblcadastrar
-            // 
-            this.lblcadastrar.AutoSize = true;
-            this.lblcadastrar.Location = new System.Drawing.Point(355, 375);
-            this.lblcadastrar.Name = "lblcadastrar";
-            this.lblcadastrar.Size = new System.Drawing.Size(0, 13);
-            this.lblcadastrar.TabIndex = 15;
-            // 
-            // lblsenha
-            // 
-            this.lblsenha.AutoSize = true;
-            this.lblsenha.BackColor = System.Drawing.Color.Transparent;
-            this.lblsenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblsenha.ForeColor = System.Drawing.Color.Transparent;
-            this.lblsenha.Location = new System.Drawing.Point(175, 269);
-            this.lblsenha.Name = "lblsenha";
-            this.lblsenha.Size = new System.Drawing.Size(100, 31);
-            this.lblsenha.TabIndex = 14;
-            this.lblsenha.Text = "Senha:";
-            // 
-            // lblcpf
-            // 
-            this.lblcpf.AutoSize = true;
-            this.lblcpf.BackColor = System.Drawing.Color.Transparent;
-            this.lblcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblcpf.ForeColor = System.Drawing.Color.White;
-            this.lblcpf.Location = new System.Drawing.Point(175, 207);
-            this.lblcpf.Name = "lblcpf";
-            this.lblcpf.Size = new System.Drawing.Size(77, 31);
-            this.lblcpf.TabIndex = 13;
-            this.lblcpf.Text = "CPF:";
-            // 
-            // txbcpf
-            // 
-            this.txbcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbcpf.Location = new System.Drawing.Point(281, 202);
-            this.txbcpf.Name = "txbcpf";
-            this.txbcpf.Size = new System.Drawing.Size(351, 47);
-            this.txbcpf.TabIndex = 12;
-            // 
-            // txbsenha
-            // 
-            this.txbsenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbsenha.Location = new System.Drawing.Point(281, 260);
-            this.txbsenha.Name = "txbsenha";
-            this.txbsenha.Size = new System.Drawing.Size(351, 47);
-            this.txbsenha.TabIndex = 11;
-            // 
-            // btnentrar
-            // 
-            this.btnentrar.Location = new System.Drawing.Point(281, 316);
-            this.btnentrar.Name = "btnentrar";
-            this.btnentrar.Size = new System.Drawing.Size(351, 52);
-            this.btnentrar.TabIndex = 10;
-            this.btnentrar.Text = "Entrar";
-            this.btnentrar.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(861, 474);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.linkblcadastrar);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblcadastrar);
-            this.Controls.Add(this.lblsenha);
-            this.Controls.Add(this.lblcpf);
-            this.Controls.Add(this.txbcpf);
-            this.Controls.Add(this.txbsenha);
-            this.Controls.Add(this.btnentrar);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Login";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.LinkLabel linkblcadastrar;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label lblcadastrar;
-        private System.Windows.Forms.Label lblsenha;
-        private System.Windows.Forms.Label lblcpf;
-        private System.Windows.Forms.TextBox txbcpf;
-        private System.Windows.Forms.TextBox txbsenha;
-        private System.Windows.Forms.Button btnentrar;
     }
 }
-
--- Pizzaria/Program.cs
+++ Pizzaria/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pizzaria;
 
 namespace Pizzaria
 {
@@ -16,7 +17,7 @@ namespace Pizzaria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forms1());
+            Application.Run(new Login());
         }
     }
 }
