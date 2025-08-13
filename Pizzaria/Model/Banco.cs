7d01aa95cf8f69b9ec02b90feab74648bdab93d4
SERGIO OCTAVIO MELO DOS SANTOS <sergio.omsantos@senacsp.edu.br>
2025-08-13 15:13:20 -0300

    Classe Banco.cs e instalaçao do MySqlConnection

--- Pizzaria/Model/Banco.cs
+++ Pizzaria/Model/Banco.cs
@@ -1,20 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlConnector;
 
 namespace Pizzaria.Model
 {
     internal class Banco
     {
         //Informações da conexão com o BD
-        private const string SERVER = "Localhost",
-                              PORT = "",
+        private const string SERVER = "10.141.44.133",
+                              PORT = "3306",
                               DATABASE = "pizzaria",
                               UID = "root",
                               PWD = "";
 
+        // Conecta ao BD
 
+        public MySqlConnection ObterConexao()
+        {
+            MySqlConnection con = null;
+            try
+            {
+                con = new MySqlConnection("SERVER=" + SERVER + ";PORT=" + PORT + ";DATABASE=" + DATABASE + ";UID=" + UID + ";PWD=" + PWD + ";");
+                con.Open();
+            }
+            catch(MySqlException e) 
+            {
+                Console.WriteLine(e.ToString());
+
+                Console.WriteLine("Não foi possivel realizar a conexão. ");
+            }
+            return con;
+        }
+
+        //Verificar se a conexão esta aberta
+        public bool ConexaoAberta(MySqlConnection con)
+        {
+            return (con.State == ConnectionState.Open);
+        }
+
+        // Metodo para desconectar
+        public void  Desconectar(MySqlConnection con)
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                Console.WriteLine("Não foi possivel encerrar a conexão. ");
+            }
+        }
     }
 }
