458ffd4b641f3a47fb752761fee7eefaae0b8a8f
Manoel <manelvitor882@gmail.com>
2025-08-14 13:55:23 -0300

    Tela de gestão de caixa

---
+++ Pizzaria/FrmGestaoCaixa.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Pizzaria
+{
+    partial class FrmGestaoCaixa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.grbPagamentos = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbFormaPagamento = new System.Windows.Forms.ComboBox();
+            this.txbValor = new System.Windows.Forms.TextBox();
+            this.lblValorFinal = new System.Windows.Forms.Label();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.grbInformacoes = new System.Windows.Forms.GroupBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.lblMesa = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txbMesa = new System.Windows.Forms.TextBox();
+            this.dgvComanda = new System.Windows.Forms.DataGridView();
+            this.grbPagamentos.SuspendLayout();
+            this.grbInformacoes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComanda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(12, 386);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(320, 49);
+            this.btnVoltar.TabIndex = 15;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // grbPagamentos
+            // 
+            this.grbPagamentos.Controls.Add(this.label1);
+            this.grbPagamentos.Controls.Add(this.cmbFormaPagamento);
+            this.grbPagamentos.Controls.Add(this.txbValor);
+            this.grbPagamentos.Controls.Add(this.lblValorFinal);
+            this.grbPagamentos.Controls.Add(this.btnFinalizar);
+            this.grbPagamentos.Location = new System.Drawing.Point(-6, 211);
+            this.grbPagamentos.Name = "grbPagamentos";
+            this.grbPagamentos.Size = new System.Drawing.Size(344, 169);
+            this.grbPagamentos.TabIndex = 17;
+            this.grbPagamentos.TabStop = false;
+            this.grbPagamentos.Text = "Pagamento";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Forma de Pagamento:";
+            // 
+            // cmbFormaPagamento
+            // 
+            this.cmbFormaPagamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbFormaPagamento.FormattingEnabled = true;
+            this.cmbFormaPagamento.Location = new System.Drawing.Point(187, 80);
+            this.cmbFormaPagamento.Name = "cmbFormaPagamento";
+            this.cmbFormaPagamento.Size = new System.Drawing.Size(139, 26);
+            this.cmbFormaPagamento.TabIndex = 10;
+            // 
+            // txbValor
+            // 
+            this.txbValor.Enabled = false;
+            this.txbValor.Location = new System.Drawing.Point(187, 39);
+            this.txbValor.Multiline = true;
+            this.txbValor.Name = "txbValor";
+            this.txbValor.Size = new System.Drawing.Size(139, 25);
+            this.txbValor.TabIndex = 7;
+            // 
+            // lblValorFinal
+            // 
+            this.lblValorFinal.AutoSize = true;
+            this.lblValorFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorFinal.Location = new System.Drawing.Point(61, 44);
+            this.lblValorFinal.Name = "lblValorFinal";
+            this.lblValorFinal.Size = new System.Drawing.Size(108, 20);
+            this.lblValorFinal.TabIndex = 8;
+            this.lblValorFinal.Text = "Valor a pagar:";
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizar.Location = new System.Drawing.Point(18, 112);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(320, 49);
+            this.btnFinalizar.TabIndex = 9;
+            this.btnFinalizar.Text = "Finalizar pedido";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            // 
+            // grbInformacoes
+            // 
+            this.grbInformacoes.Controls.Add(this.btnPesquisar);
+            this.grbInformacoes.Controls.Add(this.lblMesa);
+            this.grbInformacoes.Controls.Add(this.btnLimpar);
+            this.grbInformacoes.Controls.Add(this.txbMesa);
+            this.grbInformacoes.Location = new System.Drawing.Point(-6, 12);
+            this.grbInformacoes.Name = "grbInformacoes";
+            this.grbInformacoes.Size = new System.Drawing.Size(344, 193);
+            this.grbInformacoes.TabIndex = 16;
+            this.grbInformacoes.TabStop = false;
+            this.grbInformacoes.Text = "Informações";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(6, 69);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(320, 49);
+            this.btnPesquisar.TabIndex = 13;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // lblMesa
+            // 
+            this.lblMesa.AutoSize = true;
+            this.lblMesa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMesa.Location = new System.Drawing.Point(35, 26);
+            this.lblMesa.Name = "lblMesa";
+            this.lblMesa.Size = new System.Drawing.Size(134, 20);
+            this.lblMesa.TabIndex = 6;
+            this.lblMesa.Text = "Numero da Mesa:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(6, 124);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(320, 49);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // txbMesa
+            // 
+            this.txbMesa.Location = new System.Drawing.Point(175, 21);
+            this.txbMesa.Multiline = true;
+            this.txbMesa.Name = "txbMesa";
+            this.txbMesa.Size = new System.Drawing.Size(135, 25);
+            this.txbMesa.TabIndex = 4;
+            // 
+            // dgvComanda
+            // 
+            this.dgvComanda.AllowUserToAddRows = false;
+            this.dgvComanda.AllowUserToDeleteRows = false;
+            this.dgvComanda.AllowUserToResizeColumns = false;
+            this.dgvComanda.AllowUserToResizeRows = false;
+            this.dgvComanda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvComanda.Location = new System.Drawing.Point(358, 12);
+            this.dgvComanda.Name = "dgvComanda";
+            this.dgvComanda.Size = new System.Drawing.Size(449, 426);
+            this.dgvComanda.TabIndex = 14;
+            // 
+            // FrmGestaoCaixa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 450);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.grbPagamentos);
+            this.Controls.Add(this.grbInformacoes);
+            this.Controls.Add(this.dgvComanda);
+            this.Name = "FrmGestaoCaixa";
+            this.Text = "FrmGestaoCaixa";
+            this.grbPagamentos.ResumeLayout(false);
+            this.grbPagamentos.PerformLayout();
+            this.grbInformacoes.ResumeLayout(false);
+            this.grbInformacoes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComanda)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.GroupBox grbPagamentos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFormaPagamento;
+        private System.Windows.Forms.TextBox txbValor;
+        private System.Windows.Forms.Label lblValorFinal;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.GroupBox grbInformacoes;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Label lblMesa;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txbMesa;
+        private System.Windows.Forms.DataGridView dgvComanda;
+    }
+}
