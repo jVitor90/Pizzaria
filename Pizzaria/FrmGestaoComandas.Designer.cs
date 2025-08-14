24da8a50d6c8928d962908864fe51c5b166fa0de
GUSTAVO MELO DE LIMA <gustavo.mlima10@senacsp.edu.br>
2025-08-14 14:43:02 -0300

    Interface de gestão de comandas

--- Pizzaria/FrmGestaoComandas.Designer.cs
+++ Pizzaria/FrmGestaoComandas.Designer.cs
@@ -28,12 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grbPedidos = new System.Windows.Forms.GroupBox();
+            this.lblAdicionais = new System.Windows.Forms.Label();
+            this.cmbAdicionais = new System.Windows.Forms.ComboBox();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.lblBordas = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.cmdPizzas = new System.Windows.Forms.ComboBox();
+            this.lblPIzzas = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblObservacao = new System.Windows.Forms.Label();
+            this.cmdBebidas = new System.Windows.Forms.ComboBox();
+            this.txbObservecao = new System.Windows.Forms.TextBox();
+            this.txbMesa = new System.Windows.Forms.TextBox();
+            this.lblBebidas = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbPedidos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbPedidos
+            // 
+            this.grbPedidos.BackColor = System.Drawing.Color.Transparent;
+            this.grbPedidos.Controls.Add(this.lblAdicionais);
+            this.grbPedidos.Controls.Add(this.cmbAdicionais);
+            this.grbPedidos.Controls.Add(this.btnContinuar);
+            this.grbPedidos.Controls.Add(this.comboBox1);
+            this.grbPedidos.Controls.Add(this.btnVoltar);
+            this.grbPedidos.Controls.Add(this.lblBordas);
+            this.grbPedidos.Controls.Add(this.btnLimpar);
+            this.grbPedidos.Controls.Add(this.cmdPizzas);
+            this.grbPedidos.Controls.Add(this.lblPIzzas);
+            this.grbPedidos.Controls.Add(this.label2);
+            this.grbPedidos.Controls.Add(this.lblObservacao);
+            this.grbPedidos.Controls.Add(this.cmdBebidas);
+            this.grbPedidos.Controls.Add(this.txbObservecao);
+            this.grbPedidos.Controls.Add(this.txbMesa);
+            this.grbPedidos.Controls.Add(this.lblBebidas);
+            this.grbPedidos.ForeColor = System.Drawing.Color.Black;
+            this.grbPedidos.Location = new System.Drawing.Point(12, 34);
+            this.grbPedidos.Name = "grbPedidos";
+            this.grbPedidos.Size = new System.Drawing.Size(449, 404);
+            this.grbPedidos.TabIndex = 13;
+            this.grbPedidos.TabStop = false;
+            this.grbPedidos.Text = "Pedidos";
+            // 
+            // lblAdicionais
+            // 
+            this.lblAdicionais.AutoSize = true;
+            this.lblAdicionais.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdicionais.ForeColor = System.Drawing.Color.Black;
+            this.lblAdicionais.Location = new System.Drawing.Point(6, 164);
+            this.lblAdicionais.Name = "lblAdicionais";
+            this.lblAdicionais.Size = new System.Drawing.Size(72, 17);
+            this.lblAdicionais.TabIndex = 16;
+            this.lblAdicionais.Text = "Adicionais";
+            // 
+            // cmbAdicionais
+            // 
+            this.cmbAdicionais.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAdicionais.FormattingEnabled = true;
+            this.cmbAdicionais.Location = new System.Drawing.Point(9, 184);
+            this.cmbAdicionais.Name = "cmbAdicionais";
+            this.cmbAdicionais.Size = new System.Drawing.Size(195, 28);
+            this.cmbAdicionais.TabIndex = 15;
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.BackColor = System.Drawing.Color.White;
+            this.btnContinuar.FlatAppearance.BorderSize = 0;
+            this.btnContinuar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnContinuar.ForeColor = System.Drawing.Color.Black;
+            this.btnContinuar.Location = new System.Drawing.Point(121, 341);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(174, 32);
+            this.btnContinuar.TabIndex = 1;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(229, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(195, 28);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.BackColor = System.Drawing.Color.White;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnVoltar.ForeColor = System.Drawing.Color.Black;
+            this.btnVoltar.Location = new System.Drawing.Point(232, 303);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(174, 32);
+            this.btnVoltar.TabIndex = 0;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // lblBordas
+            // 
+            this.lblBordas.AutoSize = true;
+            this.lblBordas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBordas.ForeColor = System.Drawing.Color.Black;
+            this.lblBordas.Location = new System.Drawing.Point(226, 96);
+            this.lblBordas.Name = "lblBordas";
+            this.lblBordas.Size = new System.Drawing.Size(53, 17);
+            this.lblBordas.TabIndex = 13;
+            this.lblBordas.Text = "Bordas";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.FlatAppearance.BorderSize = 0;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpar.ForeColor = System.Drawing.Color.Black;
+            this.btnLimpar.Location = new System.Drawing.Point(12, 303);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(174, 32);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            // 
+            // cmdPizzas
+            // 
+            this.cmdPizzas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdPizzas.FormattingEnabled = true;
+            this.cmdPizzas.Location = new System.Drawing.Point(6, 39);
+            this.cmdPizzas.Name = "cmdPizzas";
+            this.cmdPizzas.Size = new System.Drawing.Size(195, 28);
+            this.cmdPizzas.TabIndex = 2;
+            // 
+            // lblPIzzas
+            // 
+            this.lblPIzzas.AutoSize = true;
+            this.lblPIzzas.BackColor = System.Drawing.Color.Transparent;
+            this.lblPIzzas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPIzzas.ForeColor = System.Drawing.Color.Black;
+            this.lblPIzzas.Location = new System.Drawing.Point(6, 16);
+            this.lblPIzzas.Name = "lblPIzzas";
+            this.lblPIzzas.Size = new System.Drawing.Size(49, 17);
+            this.lblPIzzas.TabIndex = 5;
+            this.lblPIzzas.Text = "Pizzas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(6, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "N° Da Mesa";
+            // 
+            // lblObservacao
+            // 
+            this.lblObservacao.AutoSize = true;
+            this.lblObservacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObservacao.ForeColor = System.Drawing.Color.Black;
+            this.lblObservacao.Location = new System.Drawing.Point(9, 243);
+            this.lblObservacao.Name = "lblObservacao";
+            this.lblObservacao.Size = new System.Drawing.Size(92, 17);
+            this.lblObservacao.TabIndex = 7;
+            this.lblObservacao.Text = "Observações";
+            // 
+            // cmdBebidas
+            // 
+            this.cmdBebidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdBebidas.FormattingEnabled = true;
+            this.cmdBebidas.Location = new System.Drawing.Point(229, 39);
+            this.cmdBebidas.Name = "cmdBebidas";
+            this.cmdBebidas.Size = new System.Drawing.Size(195, 28);
+            this.cmdBebidas.TabIndex = 4;
+            // 
+            // txbObservecao
+            // 
+            this.txbObservecao.Location = new System.Drawing.Point(9, 263);
+            this.txbObservecao.Multiline = true;
+            this.txbObservecao.Name = "txbObservecao";
+            this.txbObservecao.Size = new System.Drawing.Size(397, 34);
+            this.txbObservecao.TabIndex = 3;
+            // 
+            // txbMesa
+            // 
+            this.txbMesa.Location = new System.Drawing.Point(9, 118);
+            this.txbMesa.Multiline = true;
+            this.txbMesa.Name = "txbMesa";
+            this.txbMesa.Size = new System.Drawing.Size(195, 28);
+            this.txbMesa.TabIndex = 9;
+            // 
+            // lblBebidas
+            // 
+            this.lblBebidas.AutoSize = true;
+            this.lblBebidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBebidas.ForeColor = System.Drawing.Color.Black;
+            this.lblBebidas.Location = new System.Drawing.Point(226, 16);
+            this.lblBebidas.Name = "lblBebidas";
+            this.lblBebidas.Size = new System.Drawing.Size(59, 17);
+            this.lblBebidas.TabIndex = 6;
+            this.lblBebidas.Text = "Bebidas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Magneto", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(7, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Comandas";
+            // 
+            // FrmGestaoComandas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbPedidos);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmGestaoComandas";
             this.Text = "FrmGestaoComandas";
+            this.grbPedidos.ResumeLayout(false);
+            this.grbPedidos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbPedidos;
+        private System.Windows.Forms.Label lblAdicionais;
+        private System.Windows.Forms.ComboBox cmbAdicionais;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Label lblBordas;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ComboBox cmdPizzas;
+        private System.Windows.Forms.Label lblPIzzas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblObservacao;
+        private System.Windows.Forms.ComboBox cmdBebidas;
+        private System.Windows.Forms.TextBox txbObservecao;
+        private System.Windows.Forms.TextBox txbMesa;
+        private System.Windows.Forms.Label lblBebidas;
+        private System.Windows.Forms.Label label1;
     }
 }
