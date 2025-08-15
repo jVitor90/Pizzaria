a4cc3b688f57e7e26dec349a15c556b0c5f49899
SERGIO OCTAVIO MELO DOS SANTOS <sergio.omsantos@senacsp.edu.br>
2025-08-15 17:03:22 -0300

    Modificação na classe Itens_comandas

--- Pizzaria/Model/Itens_comandas.cs
+++ Pizzaria/Model/Itens_comandas.cs
@@ -8,5 +8,11 @@ namespace Pizzaria.Model
 {
     internal class Itens_comandas
     {
+        public int id_item {  get; set; }
+        public int id_Comanda { get; set; }
+        public int id_produto { get; set; }
+        public int quantidade { get; set; }
+        public string situacao_pedido { get; set; }
+
     }
 }
