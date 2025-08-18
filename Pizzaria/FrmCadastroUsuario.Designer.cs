b0023d334fe491b8e3c0511a50b972a0bf6b7ab2
GUSTAVO MELO DE LIMA <gustavo.mlima10@senacsp.edu.br>
2025-08-18 13:44:20 -0300

    Criação da interface de cadastro de usuario

---
+++ Pizzaria/FrmCadastroUsuario.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Pizzaria
+{
+    partial class FrmCadastroUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.cmbCargo = new System.Windows.Forms.ComboBox();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.txbcpf = new System.Windows.Forms.MaskedTextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.btrnCadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.BackColor = System.Drawing.Color.Transparent;
+            this.lblLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.ForeColor = System.Drawing.Color.White;
+            this.lblLogo.Location = new System.Drawing.Point(90, 13);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(214, 30);
+            this.lblLogo.TabIndex = 41;
+            this.lblLogo.Text = "Tela De Cadastro";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.BackColor = System.Drawing.Color.Transparent;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.Color.White;
+            this.lblNome.Location = new System.Drawing.Point(144, 91);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(94, 31);
+            this.lblNome.TabIndex = 40;
+            this.lblNome.Text = "Nome:";
+            // 
+            // txbNome
+            // 
+            this.txbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNome.Location = new System.Drawing.Point(244, 82);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(351, 47);
+            this.txbNome.TabIndex = 39;
+            // 
+            // cmbCargo
+            // 
+            this.cmbCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCargo.FormattingEnabled = true;
+            this.cmbCargo.Location = new System.Drawing.Point(246, 223);
+            this.cmbCargo.Name = "cmbCargo";
+            this.cmbCargo.Size = new System.Drawing.Size(351, 47);
+            this.cmbCargo.TabIndex = 38;
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.BackColor = System.Drawing.Color.Transparent;
+            this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCargo.ForeColor = System.Drawing.Color.White;
+            this.lblCargo.Location = new System.Drawing.Point(144, 232);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(96, 31);
+            this.lblCargo.TabIndex = 37;
+            this.lblCargo.Text = "Cargo:";
+            // 
+            // txbcpf
+            // 
+            this.txbcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbcpf.Location = new System.Drawing.Point(246, 153);
+            this.txbcpf.Mask = "000,000,000-00";
+            this.txbcpf.Name = "txbcpf";
+            this.txbcpf.Size = new System.Drawing.Size(351, 47);
+            this.txbcpf.TabIndex = 36;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.BackColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.Transparent;
+            this.lblSenha.Location = new System.Drawing.Point(140, 291);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(100, 31);
+            this.lblSenha.TabIndex = 35;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.BackColor = System.Drawing.Color.Transparent;
+            this.lblCpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpf.ForeColor = System.Drawing.Color.White;
+            this.lblCpf.Location = new System.Drawing.Point(161, 162);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(77, 31);
+            this.lblCpf.TabIndex = 34;
+            this.lblCpf.Text = "CPF:";
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSenha.Location = new System.Drawing.Point(246, 291);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(351, 47);
+            this.txbSenha.TabIndex = 33;
+            // 
+            // btrnCadastrar
+            // 
+            this.btrnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btrnCadastrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btrnCadastrar.Location = new System.Drawing.Point(244, 362);
+            this.btrnCadastrar.Name = "btrnCadastrar";
+            this.btrnCadastrar.Size = new System.Drawing.Size(351, 52);
+            this.btrnCadastrar.TabIndex = 32;
+            this.btrnCadastrar.Text = "Cadastrar";
+            this.btrnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // FrmCadastroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Pizzaria.Properties.Resources.pizza_4952508_1920__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(842, 450);
+            this.Controls.Add(this.lblLogo);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.cmbCargo);
+            this.Controls.Add(this.lblCargo);
+            this.Controls.Add(this.txbcpf);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblCpf);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.btrnCadastrar);
+            this.Name = "FrmCadastroUsuario";
+            this.Text = "FrmCadastroUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.ComboBox cmbCargo;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.MaskedTextBox txbcpf;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.Button btrnCadastrar;
+    }
+}
