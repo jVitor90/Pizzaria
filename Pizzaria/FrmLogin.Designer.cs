6ef563d804b9b961e097d9cab1721ce70d5c1717
Sergiosantos3 <tavinhomelo69@gmail.com>
2025-08-20 14:20:22 -0300

    Resolução de Erros

--- Pizzaria/FrmLogin.Designer.cs
+++ Pizzaria/FrmLogin.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class FrmLogin
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+     
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,21 +17,14 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+     
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
+
             this.lblcadastrar = new System.Windows.Forms.Label();
->>>>>>> Stashed changes
+
             this.lblsenha = new System.Windows.Forms.Label();
             this.lblcpf = new System.Windows.Forms.Label();
             this.txbsenha = new System.Windows.Forms.TextBox();
@@ -44,9 +32,7 @@
             this.txbcpf = new System.Windows.Forms.MaskedTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
+           
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(350, 23);
@@ -55,23 +41,13 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
-            // 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-            // lblcadastrar
-            // 
+ 
             this.lblcadastrar.AutoSize = true;
             this.lblcadastrar.Location = new System.Drawing.Point(327, 363);
             this.lblcadastrar.Name = "lblcadastrar";
             this.lblcadastrar.Size = new System.Drawing.Size(0, 13);
             this.lblcadastrar.TabIndex = 15;
-            // 
->>>>>>> Stashed changes
-            // lblsenha
-            // 
+
             this.lblsenha.AutoSize = true;
             this.lblsenha.BackColor = System.Drawing.Color.Transparent;
             this.lblsenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -81,9 +57,7 @@
             this.lblsenha.Size = new System.Drawing.Size(100, 31);
             this.lblsenha.TabIndex = 14;
             this.lblsenha.Text = "Senha:";
-            // 
-            // lblcpf
-            // 
+        
             this.lblcpf.AutoSize = true;
             this.lblcpf.BackColor = System.Drawing.Color.Transparent;
             this.lblcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -93,18 +67,14 @@
             this.lblcpf.Size = new System.Drawing.Size(77, 31);
             this.lblcpf.TabIndex = 13;
             this.lblcpf.Text = "CPF:";
-            // 
-            // txbsenha
-            // 
+         
             this.txbsenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbsenha.Location = new System.Drawing.Point(253, 248);
             this.txbsenha.Name = "txbsenha";
             this.txbsenha.PasswordChar = '•';
             this.txbsenha.Size = new System.Drawing.Size(351, 47);
             this.txbsenha.TabIndex = 11;
-            // 
-            // btnentrar
-            // 
+        
             this.btnentrar.Location = new System.Drawing.Point(253, 304);
             this.btnentrar.Name = "btnentrar";
             this.btnentrar.Size = new System.Drawing.Size(351, 52);
@@ -112,24 +82,14 @@
             this.btnentrar.Text = "Entrar";
             this.btnentrar.UseVisualStyleBackColor = true;
             this.btnentrar.Click += new System.EventHandler(this.btnentrar_Click);
-            // 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-            // txbcpf
-            // 
+
             this.txbcpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbcpf.Location = new System.Drawing.Point(253, 195);
             this.txbcpf.Mask = "000,000,000-00";
             this.txbcpf.Name = "txbcpf";
             this.txbcpf.Size = new System.Drawing.Size(351, 47);
             this.txbcpf.TabIndex = 19;
-            // 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-            // FrmLogin
-            // 
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
@@ -137,13 +97,9 @@
             this.ClientSize = new System.Drawing.Size(842, 450);
             this.Controls.Add(this.txbcpf);
             this.Controls.Add(this.pictureBox1);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
+
             this.Controls.Add(this.lblcadastrar);
->>>>>>> Stashed changes
+
             this.Controls.Add(this.lblsenha);
             this.Controls.Add(this.lblcpf);
             this.Controls.Add(this.txbsenha);
@@ -160,13 +116,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
+
         private System.Windows.Forms.Label lblcadastrar;
->>>>>>> Stashed changes
+
         private System.Windows.Forms.Label lblsenha;
         private System.Windows.Forms.Label lblcpf;
         private System.Windows.Forms.TextBox txbsenha;
